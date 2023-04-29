2467de0eef55819c87019891766fd202bda4e831
akuldeepj <akuldeepj@gmail.com>
2023-04-29 15:04:03 +0530

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+
+# import numpy as np
+# mat1,mat2 = ([1, 6, 5],[3 ,4, 8],[2, 12, 3]),([3, 4, 6],[5, 6, 7],[6,56, 7])
+# print(np.dot(mat1,mat2))
+
+
+import numpy as np
+import numdifftools as nd
+
